28f0c33da92142f0977483268ef15283277134f0
Srbuhi Mirzoyan <38066772+srbuhimirzoyan@users.noreply.github.com>
2019-12-24 17:36:07 +0400

    Add files via upload

---
+++ Dataset_merging/config.py
@@ -0,0 +1,4 @@
+
+#These districts are going to be used in location_change function in utils.py
+districts=["Կենտրոն","Արաբկիր","Շենգավիթ","Մալաթիա Սեբաստիա","Նոր Նորք","Էրեբունի","Քանաքեռ Զեյթուն","Աջափնյակ","Ավան","Դավիթաշեն",
+"Նորք Մարաշ","Ջրվեժ"]
